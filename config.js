2a5f404ffbb46f26630e5ac5ea74841763ff1c7f
Ptkhacker <pridekusvabadika@gmail.com>
2025-07-09 21:12:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ DEBUG =
 module.exports = {
   VERSION: "v1.2.8",
   SESSION:
-    process.env.ASENA_SESSION === undefined ? "" : process.env.ASENA_SESSION,
+    process.env.ASENA_SESSION === undefined ? "levanter_1063390f6f36f341dca35bb4fe4a6f3682" : process.env.ASENA_SESSION,
   EXT: process.env.EXT === undefined ? undefined : process.env.EXT,
   LANG:
     process.env.LANGUAGE === undefined
